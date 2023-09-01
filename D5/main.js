9a00548684a2f2de88afe6639372284fda2df724
SiMoZ9 <simonejonas08@gmail.com>
2023-09-01 18:09:08 +0200

    some stuff

--- D5/main.js
+++ D5/main.js
@@ -1,27 +1,46 @@
-const fetchMusic = (query, id) => {
-  const section = document.querySelector(`#${id}`)
+const id = 'song'
+const getRow = () => document.querySelector(`#${id}Section`)
 
-  const row = document.querySelector(`#${id}Section`)
-  console.log(row)
+const fetchMusic = (query, id) => {
+  /*
+    Prima della ricerca dinamicizzata gli id degli artisti era già definiti nell'index
+  */
+  const section = document.querySelector(`#${id}`)
   fetch(
     "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + query
   )
     .then((raw) => {
-      console.log('raw',raw);      
+      if (raw.ok) return raw.json()
+
+      else throw new Error(raw.statusText)
+      
     })
     .then((res) => {
       let music = res.data
       section.classList.remove("d-none")
       for (let i = 0; i < music.length; i++) {
         const element = music[i]
-        row.innerHTML += `<div class='col col-3'> <a href="detail.html?title=${element.title}&artist=${element.artist.name}"><img class='w-100' src='${element.album.cover_xl}'/> </a></div>`
+
+        /*
+          Aggiugo alla section precedente, quello scritto innerHTML
+        */
+
+        getRow().innerHTML += `<div class='col col-3'> <a href="detail.html?title=${element.title}&artist=${element.artist.name}"><img class='w-100' src='${element.album.cover_xl}'/> </a></div>`
       }
     })
-    .catch((err) => console.log(err))
+    .catch((err) => {throw new Error(err)})
 }
 
-window.onload = () => {
+const search = () => {
+  //prendo il value dell'input
+  getRow().innerHTML = '';
+  const query = document.getElementById("searchField").value;
+  fetchMusic(query, 'song');
+  document.getElementById('titolo').innerText = query;
+}
+
+/* window.onload = () => {
 
   fetchMusic("queen", "queen")
   fetchMusic("metallica", "metallica")
-}
+} */
